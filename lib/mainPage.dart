b2567432f823945de82a401ac10cdc15cebe9ac9
XY7016 <yuanzq1992@163.com>
2019-11-22 20:26:08 +0800

    Initial commit

---
+++ lib/mainPage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyMainPageApp());
+}
+
+class MyMainPageApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "主页",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+
+class MainPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 2560,
+        height: 3440,
+        color: Colors.blue,
+        child: Image.asset("images/img_android_logo.png"),
+      ),
+    );
+  }
+
+}
